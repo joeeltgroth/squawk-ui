9ef14695944dda1e8f99208e4219d1303ecb7f4a
Joe Eltgroth <joe.eltgroth@gmail.com>
2021-10-05 21:54:42 -0500

    Some flying club verbiage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import './App.css';
-import {Button, Container, Toast} from "react-bootstrap";
-import {useState} from "react";
-
-const ExampleToast = ({children}) => {
-    const [show, toggleShow] = useState(true);
-
-    return (
-        <>
-            {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-            <Toast show={show} onClose={() => toggleShow(false)}>
-                <Toast.Header>
-                    <strong className="mr-auto">React-Bootstrap</strong>
-                </Toast.Header>
-                <Toast.Body>{children}</Toast.Body>
-            </Toast>
-        </>
-    );
-};
+import {Container, Accordion, Card} from "react-bootstrap";
 
 function App() {
     return (
         <Container className="p-3">
-            <h1 className="header">Flying Club</h1>
-            <ExampleToast>
-                We now have Toasts
-                <span role="img" aria-label="tada">🎉</span>
-            </ExampleToast>
+            <h1 className="header">Airplane Maintenance and Operation Notes</h1>
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Archer N29265</Accordion.Header>
+                    <Accordion.Body>
+                        <Card>
+                            <Card.Body>Trim button on yoke inoperable.</Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Body>Last oil added 9/23 at 2388 tach hours.</Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Body>Last oil added 9/23 at 2388 tach hours.</Card.Body>
+                        </Card>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Tomahawk N9741T</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </Container>
     );
 }
