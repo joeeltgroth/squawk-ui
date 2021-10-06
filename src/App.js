2a506c5fd16d2ed4b4e91233b2af526730de74d4
Joe Eltgroth <joe.eltgroth@gmail.com>
2021-10-05 21:40:02 -0500

    Initial example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Container, Toast} from "react-bootstrap";
+import {useState} from "react";
+
+const ExampleToast = ({children}) => {
+    const [show, toggleShow] = useState(true);
+
+    return (
+        <>
+            {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+            <Toast show={show} onClose={() => toggleShow(false)}>
+                <Toast.Header>
+                    <strong className="mr-auto">React-Bootstrap</strong>
+                </Toast.Header>
+                <Toast.Body>{children}</Toast.Body>
+            </Toast>
+        </>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container className="p-3">
+            <h1 className="header">Flying Club</h1>
+            <ExampleToast>
+                We now have Toasts
+                <span role="img" aria-label="tada">🎉</span>
+            </ExampleToast>
+        </Container>
+    );
 }
 
 export default App;
