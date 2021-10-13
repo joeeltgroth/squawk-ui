4b05d3819095eb6327de6607058e7d317296aed9
Joe Eltgroth <joe.eltgroth@gmail.com>
2021-10-12 20:53:41 -0500

    Some UI ideas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
-import {Container, Accordion, Card} from "react-bootstrap";
+import {Container, Accordion, Card, Navbar, Nav, NavDropdown, Button} from "react-bootstrap";
 
 function App() {
     return (
         <Container className="p-3">
-            <h1 className="header">Airplane Maintenance and Operation Notes</h1>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Airplane Operation Notes</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#notes">Notes</Nav.Link>
+                            <Nav.Link href="#notes">Members</Nav.Link>
+                            <Nav.Link href="#notes">Weight/Balance</Nav.Link>
+                            <NavDropdown title="Links" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="https://www.goboko.com/">Goboko</NavDropdown.Item>
+                                <NavDropdown.Item href="https://www.facebook.com/groups/2296761510590369">Facebook Site</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Navbar.Text>
+                            Signed in as: <a href="#login">Joe</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div></div>
             <Accordion>
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>Archer N29265</Accordion.Header>
@@ -18,6 +39,8 @@ function App() {
                         <Card>
                             <Card.Body>Last oil added 9/23 at 2388 tach hours.</Card.Body>
                         </Card>
+                        <p>&nbsp;</p>
+                        <Button variant="outline-primary">Report Squawk</Button>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
@@ -30,6 +53,8 @@ function App() {
                         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
                         cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
                         est laborum.
+                        <p>&nbsp;</p>
+                        <Button variant="outline-primary">Report Squawk</Button>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
